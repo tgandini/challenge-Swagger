3806fd00de3d13f1807d57d5831a3b8e2d9b014b
Tomas Gandini <tomas.gandini@avalith.net>
2022-10-21 10:42:30 -0300

    add annotations para getProductById

--- src/Actions/Products/GetProductByIdAction.php
+++ src/Actions/Products/GetProductByIdAction.php
@@ -30,6 +30,33 @@ class GetProductByIdAction extends Action
      * 
      * @return \Psr\Http\Message\ResponseInterface
      */
+
+    /**
+     * @OA\Get(
+     *     path="/v1/products/{id}",
+     *     @OA\Response(
+     *         response="200",
+     *         description="Búsqueda exitosa",
+     *         @OA\JsonContent(ref="#/components/schemas/Product"),
+     *     ),
+     *    @OA\Response(
+     *        response="500",
+     *        description="Error al buscar el producto",
+     *      @OA\JsonContent(ref="#/components/schemas/Internal Server error"),
+     *   ), 
+     *     tags={"Operaciones de Lectura"},
+     *     summary="Get producto by id",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="id del Producto",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *         )
+     *     ),
+     * ) 
+     */     
     public function __invoke(Request $request, Response $response, int $id = null): Response
     {
         /**

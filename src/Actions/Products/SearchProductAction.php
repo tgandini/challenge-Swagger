212ed0c02c6166461f6d9627705ffe4a2d280efb
Tomas Gandini <tomas.gandini@avalith.net>
2022-10-21 12:04:36 -0300

    annotations Get Producto by custom Parameter

--- src/Actions/Products/SearchProductAction.php
+++ src/Actions/Products/SearchProductAction.php
@@ -23,6 +23,55 @@ class SearchProductAction extends Action
             ->getRepository(Product::class);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/v1/products/search",
+     *     tags={"Operaciones de Lectura"},
+     *     summary="Get Producto by custom Parameter",
+     *    @OA\RequestBody(
+     *        required=true,
+     *        @OA\MediaType(
+     *           mediaType="multipart/form-data",
+     *          @OA\Schema(
+     *             @OA\Property(
+     *               property="name",
+     *               description="Buscar por Nombre",
+     *               type="string"),
+     *             @OA\Property(
+     *               property="slug",
+     *               description="Buscar por Slug",
+     *               type="string"),
+     *            @OA\Property(
+     *               property="description",
+     *               description="Buscar por Descripción",
+     *               type="string"),
+     *           @OA\Property(
+     *               property="price",
+     *               description="Buscar por Precio",
+     *               type="number"),
+     *           @OA\Property(
+     *               property="stock",
+     *               description="Buscar por Stock",
+     *               type="number"),
+     *          @OA\Property(
+     *              property="keywords",
+     *              description="Buscar por Palabras Clave",
+     *             type="string")
+     *      )
+     *    )
+     *  ),
+     *   @OA\Response(
+     *        response=200,
+     *        description="Producto encontrado",
+     *        @OA\JsonContent(ref="#/components/schemas/Product"),
+     *   ),
+     *    @OA\Response(
+     *        response="404",
+     *        description="Producto no encontrado",
+     *      @OA\JsonContent(ref="#/components/schemas/Internal Server error"),
+     *   ), 
+     * ) 
+     */
     public function __invoke(Request $request, Response $response): Response
     {
         $data = $request->getParsedBody();

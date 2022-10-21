db6f1774a4c598025d23df1515cd2d960a1192ec
Tomas Gandini <tomas.gandini@avalith.net>
2022-10-21 10:21:45 -0300

    Annotations para alta de producto

--- src/Actions/Products/CreateProductAction.php
+++ src/Actions/Products/CreateProductAction.php
@@ -24,6 +24,68 @@ class CreateProductAction extends Action
             ->getRepository(Product::class);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/v1/createProduct",
+     *     tags={"Operaciones de Escritura"},
+     *     summary="Dar de alta un Producto",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="name",
+     *                     description="Nombre del producto",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="slug",
+     *                     description="Slug del producto",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="description",
+     *                     description="Descripción del producto",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="price",
+     *                     description="Precio del producto",
+     *                     type="number"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="stock",
+     *                     description="Stock del producto",
+     *                     type="number"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="keywords",
+     *                     description="Palabras clave (Keywords) del producto",
+     *                     type="string"
+     *                 ),
+     *                 required={"name", "slug", "description", "price", "stock", "keywords"}
+     *             )
+     *         )
+     *     ),
+     *    @OA\Response(
+     *        response=200,
+     *       description="Success al crear un producto",
+     *      @OA\JsonContent(
+     *       type="text/plain",
+     *     example="Product created succesfully"
+     *     )
+     *   ),
+     *  @OA\Response(
+     *     response="500",
+     *    description="Internal Server error al crear un producto",
+     *  @OA\JsonContent(
+     *    type="array",
+     *   ref="#/components/schemas/Internal Server error Alta"
+     *   )
+     * )
+     * )
+     */
     public function __invoke(Request $request, Response $response): Response
     {
         $data = $request->getParsedBody();
